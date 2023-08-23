80673229c5d70198d95cf42f30a77251897c0a25
Harsh Kain <72141797+harsh-kain@users.noreply.github.com>
2023-08-23 06:50:55 +0000

    adding react file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-
-function App() {
+import Hello from "./components/Hello";
+import './App.css'
+const App = () => {
   return (
     <div className="App">
       <h1>We are Anonymous</h1>
+      <Hello/>
     </div>
   );
 }
 
 export default App;
+
+// component -> a function which return a value 
