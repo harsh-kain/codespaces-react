7c3705c1c93e8add8698acb223df454ccc4b84b9
Harsh Kain <72141797+harsh-kain@users.noreply.github.com>
2023-09-01 17:05:04 +0000

    remove some unwanted files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
 import "./App.css";
-// import UseStateHooksEx from "./hooks/UseStateHooksEx";
-import Form from "./form/Form";
-import CheckBox from "./form/CheckBox";
-import Api from "./Api/Api";
+
 const App = () => {
   
   return (
     <>
-        
-      <div className="container-fluid bg-black-teritory text-bg-dark vh-100 d-flex justify-content-center align-items-center">
-        {/* <UseStateHooksEx/> */}
-        {/* <Form/>
-        <CheckBox/> */}
-        <Api/>
-      </div>
-
-
-
-        
+      <h1>kain</h1>
       
     </>
   );
